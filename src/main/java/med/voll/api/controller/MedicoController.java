a7da29470bcd569d5778ee444b5fd2c8aad3ca5c
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-24 19:13:21 -0300

    :recycle: MedicoController and add Especialidade Enum.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,9 +2,11 @@ package med.voll.api.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +19,9 @@ import java.util.stream.Collectors;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
 
-        String body = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        JsonParser springParser = JsonParserFactory.getJsonParser();
-        Map<String, Object> map = springParser.parseMap(body);
-
-        System.out.println(body);
-        System.out.println(map);
+        System.out.println(dados);
 
      }
 
