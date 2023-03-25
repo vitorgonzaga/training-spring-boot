d9da66f75aeff8026b01d9f06004d371df3b19a7
Vitor Ferreira <vitor.gonzagaf1@gmail.com>
2023-03-25 16:14:43 -0300

    :sparkles: create the feature to update the nome, telefone and Endereco of a Medico. Using the getReferenceById from repository is possivel throught custom methods in entity jpa set new values from data received in request. For it was necessary include id attribute into DadosListagemMedico for client-side can send it in body request according DadosAtualizacaoMedico DTO.

--- src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -1,6 +1,8 @@
 package med.voll.api.medico;
 
 public record DadosListagemMedico(
+
+        Long id,
         String nome,
         String email,
         String crm,
@@ -8,7 +10,7 @@ public record DadosListagemMedico(
 ) {
 
     public DadosListagemMedico(Medico medico) {
-        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 
 }
